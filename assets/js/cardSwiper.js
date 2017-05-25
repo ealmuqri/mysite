37ce95e8a3dc58d38ded33b9c76e9f9ccdbc08ef
Essam Almuqri <essam.almuqri@gmail.com>
2017-05-25 23:27:46 +0300

    Added mysite code

---
+++ assets/js/cardSwiper.js
@@ -0,0 +1,23 @@
+var cardSwiper={
+    // Swiper Config
+        swipeType: "linear",     // or Circular
+        lookUpCriteria: "id",    // or class
+    
+    // Swiper Globals { don't touch :) }
+        current:,
+        numberOfCards:,
+        cardsLookup:[],
+    
+    // Swiper Functions
+        
+        /* 
+        - Parses page HTML looking for cards with class name 'card-swipable'
+        - Unhides the first card.
+        - Adds custom ID or Class (look Swiper Config) to cards.
+        - Updates numberOfCards & cardsLookup variables.
+        */
+        updateNumberOfCards: function(){
+            
+        }
+    
+};
